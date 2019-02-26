60ce728c2d05553f20e36abb548f97b81d793804
tushar goel <tushar.goel.dav@gmail.com>
2019-02-26 13:45:07 +0530

    done
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

--- generator.py
+++ generator.py
@@ -3,43 +3,18 @@ import pandas as pd
 import datetime
 from jinja2 import Template
 import pdfkit
-# from slugify import slugify
+import datetime
+now = datetime.datetime.now()
 df = pd.DataFrame([
-        {'package': 'PEPTize', 'student': 'Yashvant Singh', 'level': 'Level 1'}
-        # {'package': 'PlaceMe Mechanical', 'student': 'Rajeev Kumar', 'level': 'Level 2'},
-        # {'package': 'N2N Python', 'student': 'Gaurav Singh', 'level': 'Level 2'},
-        # {'package': 'PEPTize', 'student': 'Rakesh Sharma', 'level': 'Level 3'},
+        {'package': 'PEPTize', 'student': 'Yashvant Singh', 'level': 'Level 1'},
+        {'package': 'PlaceMe Mechanical', 'student': 'Rajeev Kumar', 'level': 'Level 2'},
+        {'package': 'N2N Python', 'student': 'Gaurav Singh', 'level': 'Level 2'},
+        {'package': 'PEPTize', 'student': 'Rakesh Sharma', 'level': 'Level 3'},
     ])
-import base64
-img=''
-with open("./static/images/Background.jpg", "rb") as imageFile:
-    str = base64.b64encode(imageFile.read())
-    img=str
-ht='''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,500,600,800' rel='stylesheet'>
-		<link rel='stylesheet' href='./static/styles.css'>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Perfectice - Certificate</title>
-	</head>
-	<body>
-		<div id="background">
-			<div id="Background">
-				<img class ="img" src="https://user-images.githubusercontent.com/34160672/53386713-5207a600-39a9-11e9-8df8-74b605edf1bd.jpg">
-				<span class="main">CERTIFICATE</span>
-				<span class="main1">OF COMPLETION</span>
-				<span class="content1">This certificate is hereby presented to </span>
-				<span class="name">{{student}}</span>
-				<span class="content2">for successfully completing <b>{{level}} of {{package}}</b> assessed by Perfectice</span>
-				<span class="date">{{certdate}}</span>
-				<span class="footer">To validate or download the certificate visit https://lpu.myperfectice.com/cert/</span>
-			</div>
-		</div>
- </body>
- </html>
- '''
-template = Template(ht)
-x=template.render(student=df['student'].values[0],level=df['level'].values[0],package=df['package'].values[0],certdate='ab',img=img)
-pdfkit.from_string(x,'./output/out.pdf')
+f = open("./templates/index.html", "r")
+template = Template(f.read())
+for index,row in df.iterrows():
+    options = {'orientation': 'Landscape'}
+    x=template.render(student=row['student'],level=row['level'],package=row['package'],certdate=now.date())
+    pdfkit.from_string(x,'./output/out'+str(index)+'.pdf',options=options)
 
